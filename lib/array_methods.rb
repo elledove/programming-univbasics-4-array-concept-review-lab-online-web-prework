b17ab4ea728d4a7064175d0edd320ed1f7ac9433
Lauren Dove <elledoveinc@gmail.com>
2019-08-13 06:20:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index (array , value_to_find) # (1,2,3)
-  array.find_element_index[value_to_find]
+  array.find_index[value_to_find]
   end                                  # Add your solution here
 
 def find_max_value (array)
- array.  # Add your solution here
+  # Add your solution here
 end
 
 def find_min_value(array)
