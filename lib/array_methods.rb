d22fca5aec45006bf8c71fa34ab5b83770092d70
Lauren Dove <elledoveinc@gmail.com>
2019-08-13 06:19:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index (array , value_to_find) # (1,2,3)
-  array.index(value_to_find)
+  array.index[value_to_find]
   
 end                                  # Add your solution here
 
