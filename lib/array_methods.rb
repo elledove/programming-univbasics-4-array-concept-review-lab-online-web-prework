5ab72ee8548965b2e75b9622082e9b70ba16352d
Lauren Dove <elledoveinc@gmail.com>
2019-08-13 05:59:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index (array , value_to_find) # (1,2,3)
   array.index[]
- value_to_find[]
+ value_to_find.index[]
 end                                  # Add your solution here
 
 def find_max_value (array)
